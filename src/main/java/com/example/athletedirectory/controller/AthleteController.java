4cd3fe631d8e69080f9d645642728617c427f93e
Aayush Kumar Yadav <150145658+yayush05@users.noreply.github.com>
2024-11-18 13:46:56 +0530

    Update AthleteController.java

--- src/main/java/com/example/athletedirectory/controller/AthleteController.java
+++ src/main/java/com/example/athletedirectory/controller/AthleteController.java
@@ -1,5 +1,57 @@
 package com.example.athletedirectory.controller;
+package com.example.athletedirectory.controller;
 
+import com.example.athletedirectory.model.Athlete;
+import com.example.athletedirectory.model.Country;
+import com.example.athletedirectory.service.AthleteJpaService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.*;
+
+@RestController
+public class AthleteController {
+    @Autowired
+    private AthleteJpaService athleteJpaService;
+
+    @GetMapping("/countries/athletes")
+    public ArrayList<Athlete> getAthletes() {
+        return athleteJpaService.getAthletes();
+    }
+
+    @GetMapping("/countries/athletes/{athleteId}")
+    public Athlete getAthleteById(@PathVariable("athleteId") int athleteId) {
+        return athleteJpaService.getAthleteById(athleteId);
+    }
+
+    @PostMapping("/countries/athletes")
+    public Athlete addAthlete(@RequestBody Athlete athlete) {
+        return athleteJpaService.addAthlete(athlete);
+    }
+
+    @PutMapping("/countries/athletes/{athleteId}")
+    public Athlete updateAthlete(@PathVariable("athleteId") int athleteId, @RequestBody Athlete athlete) {
+        return athleteJpaService.updateAthlete(athleteId, athlete);
+    }
+
+    @DeleteMapping("/countries/athletes/{athleteId}")
+    public ResponseEntity<Void> deleteAthlete(@PathVariable("athleteId") int athleteId) {
+        try {
+            athleteJpaService.deleteAthlete(athleteId);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (Exception e) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping("/athletes/{athleteId}/country")
+    public Country getAthleteCountry(@PathVariable("athleteId") int athleteId) {
+        return athleteJpaService.getAthleteCountry(athleteId);
+    }
+}
 import com.example.athletedirectory.model.Athlete;
 import com.example.athletedirectory.model.Country;
 import com.example.athletedirectory.service.AthleteJpaService;
